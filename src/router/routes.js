e9dc0afc29f18b12ec5a62572803c8981f49cf7d
“[Zihadul <[geniusnaeem5@gmail.com]>
2023-06-23 15:37:00 +0600

    practice

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,21 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: "", component: () => import("pages/Registration.vue") },
+      { path: "login", component: () => import("pages/login.vue") },
+      { path: "user", component: () => import("pages/userPage.vue") },
+      { path: "adress", component: () => import("pages/usarAdress.vue") },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
